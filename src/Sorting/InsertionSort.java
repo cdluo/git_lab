5327a45eaa1fea3ac3b85d3da99f5c4f902a4f4b
Sahakait Benyasut <sahakait_benyasut@brown.edu>
2016-03-19 17:34:45 -0400

    Added insertion sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,15 +1,25 @@
 package Sorting;
 
 public class InsertionSort {
-	/**
-	 * insertionSort			sorts an array of integers using insertion sort
-	 * 
-	 * @param unsortedArray		an unsorted array of integers
-	 * @return					the input array, sorted least to greatest
-	 */
-	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
-	}
+
+
+    /**
+    * insertionSort            sorts an array of integers using insertion sort
+    * 
+    * @param unsortedArray     an unsorted array of integers
+    * @return                  the input array, sorted least to greatest
+    */
+    public static int[] insertionSort(int[] unsortedArray) {
+        for(int i = 1; i < array.length; i++){
+          int curr = array[i];
+          int j = i-1;
+          while(j>= 0 && array[j] > curr){
+            array[j + 1] = array [j];
+            j -= 1;
+          }
+         array[j+1] = curr;
+        }
+        
+        return array;
+    }
 }
